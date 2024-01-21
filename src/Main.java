2009b89dc19a04f79ba0bc9edad16243830383f3
VinitaSonwane <VinitaSonwane>
2024-01-21 21:35:55 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main{
+    public static void main(String[] agrs ){
+        //Database URL
+        String url = "jdbc:mysql://localhost:3306/Students";
+
+        //Database credentials
+        String username = "root";
+        String password = "Vinita@123";
+
+        //Establish the connection
+        try(Connection connection = DriverManager.getConnection(url,username , password)){
+            System.out.println("Connected to the database ");
+            System.out.println(connection);
+        }
+        catch(SQLException e ){
+          System.err.println("connection failed : " + e.getMessage());
+        }
+    }
+}
